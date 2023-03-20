94c7b387fbeb61c6430d87b9955da6517de9c6ca
Vlad Kovalynskyi <99043529+vladkovalynskyi@users.noreply.github.com>
2023-03-20 12:36:25 +0200

    xxxxxxxxx

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
+import './js/buy-now'
+import './js/swiper'
+// import './js/modal-review'
 // import '/js/slick.min';
 // import '/js/modal-buy-now';
-import './js/buy-now'
 // import '/js/mobile-menu'
-import './js/swiper'
-import './js/modal-review'
 // import '/js/modal-subscribe'
 // import '/js/modal';
 ///////////////////////////////////////////////

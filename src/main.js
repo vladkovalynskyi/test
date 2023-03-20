0336993dadac0136fb6fe6df637b881a262bb086
Vlad Kovalynskyi <99043529+vladkovalynskyi@users.noreply.github.com>
2023-03-20 11:31:20 +0200

    test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import './js/slick.min.js';
-import './js/modal-buy-now.js';
-import './js/buy-now.js';
-import './js/mobile-menu.js';
-import './js/swiper.js';
-import './js/modal-review.js';
-import './js/modal-subscribe.js';
-import './js/modal.js';
+import '/js/slick.min.js';
+import '/js/modal-buy-now.js';
+import '/js/buy-now.js';
+import '/js/mobile-menu.js';
+import '/js/swiper.js';
+import '/js/modal-review.js';
+import '/js/modal-subscribe.js';
+import '/js/modal.js';

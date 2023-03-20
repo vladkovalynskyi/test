0b8a24cdd84809ea0cbb3810090d542d03661756
Vlad Kovalynskyi <99043529+vladkovalynskyi@users.noreply.github.com>
2023-03-20 12:02:28 +0200

    sfdfs

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // import '/js/modal-buy-now';
 import './js/buy-now'
 // import '/js/mobile-menu';
-import '/js/swiper'
+import './js/swiper'
 // import '/js/modal-review';
 // import '/js/modal-subscribe';
 // import '/js/modal';
